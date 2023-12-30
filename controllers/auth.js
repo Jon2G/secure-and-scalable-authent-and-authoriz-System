68c686a0478ce77275d5cb792dda58503d3b40fd
Jon2G <jonyjouna@gmail.com>
2023-12-30 03:33:37 -0600

    remove email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,7 @@
 const bcrypt = require('bcrypt')
 const user = require("../models/user")
 const jwt= require('jsonwebtoken')
-const OTP = require('../models/OTP')
-const otpGenerator = require("otp-generator");
+
 require('dotenv').config()
 //signup handle
 exports.signup = async(req, res)=> {
@@ -152,50 +151,3 @@ exports.login = async(req, res)=> {
     }
 
 }
-
-// Send OTP For Email Verification
-exports.sendotp = async (req, res) => {
-	try {
-		const { email } = req.body;
-
-		// Check if user is already present
-		// Find user with provided email
-		const checkUserPresent = await user.findOne({ email });
-		// to be used in case of signup
-
-		// If user found with provided email
-		if (checkUserPresent) {
-			// Return 401 Unauthorized status code with error message
-			return res.status(401).json({
-				success: false,
-				message: `User is Already Registered`,
-			});
-		}
-
-		var otp = otpGenerator.generate(6, {
-			upperCaseAlphabets: false,
-			lowerCaseAlphabets: false,
-			specialChars: false,
-		});
-		const result = await OTP.findOne({ otp: otp });
-		console.log("Result is Generate OTP Func");
-		console.log("OTP", otp);
-		console.log("Result", result);
-		while (result) {
-			otp = otpGenerator.generate(6, {
-				upperCaseAlphabets: false,
-			});
-		}
-		const otpPayload = { email, otp };
-		const otpBody = await OTP.create(otpPayload);
-		console.log("OTP Body", otpBody);
-		res.status(200).json({
-			success: true,
-			message: `OTP Sent Successfully`,
-			otp,
-		});
-	} catch (error) {
-		console.log(error.message);
-		return res.status(500).json({ success: false, error: error.message });
-	}
-};
