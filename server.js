0248351983fe952a9532442bf1b97c9c2a803d0e
Jon2G <jonathan.edu.gar@gmail.com>
2023-12-30 02:52:36 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 process.env["MONGODB_URL"]='mongodb://tf_jslt_admin:689865541998@tf-jslt.cluster-c7aaqoyekc8g.us-east-1.docdb.amazonaws.com:27017/jslt?tls=false&tlsCAFile=./docdb-bastion.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false'
-process.env["MAIL_HOST"]=''
+process.env["MAIL_HOST"]='smtp.gmail.com'
 process.env["MAIL_USER"]=''
 process.env["MAIL_PASS"]=''
-process.env["JWT_SECRET"]=''
+process.env["JWT_SECRET"]='jslt'
 process.env["PORT"]=4000
 
 
