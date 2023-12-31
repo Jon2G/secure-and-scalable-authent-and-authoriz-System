ee2e2a5045493b4eca230a197f270bcd7c08aa58
Jon2G <jonyjouna@gmail.com>
2023-12-31 02:12:11 -0600

    Update elgamal.js

--- chiper/elgamal.js
+++ chiper/elgamal.js
@@ -1,6 +1,7 @@
 const ElGamal = require('elgamal');
 
 exports.generateKeys = async () => {
+    console.log(ElGamal)
     const eg = await ElGamal.generateAsync(); // Recommended way of initialization
     return eg;
 }
